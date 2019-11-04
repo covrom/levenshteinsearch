37f01381f6befd88d2bab1dd136ca8cb078cf2a7
Roman <covrom@gmail.com>
2019-11-04 23:15:37 +0300

    russian tests

--- pkg/levenshteinsearch/search_test.go
+++ pkg/levenshteinsearch/search_test.go
@@ -9,6 +9,9 @@ func TestSearch(t *testing.T) {
 	dict.Put("banana")
 	dict.Put("orange")
 	dict.Put("monkey")
+	dict.Put("бананы")
+	dict.Put("оранжевый")
+	dict.Put("обезьяна")
 
 	result := dict.SearchAll("banana", 1)
 	for word := range result {
@@ -30,7 +33,24 @@ func TestSearch(t *testing.T) {
 	}
 
 	result = dict.SearchAll("a", 6)
-	if len(result) != 3 {
+	if len(result) != 4 {
 		t.Error("Expected to find 'banana', 'orange' and 'monkey' with a distance of 6")
+	}
+
+	result = dict.SearchAll("банан", 1)
+	for word := range result {
+		if word != "бананы" {
+			t.Error("Expected to find 'банан' with a distance of 1")
+		}
+	}
+
+	result = dict.SearchAll("ан", 2)
+	if len(result) != 0 {
+		t.Error("Expected to find 'ан' with a distance of 2")
+	}
+
+	result = dict.SearchAll("а", 6)
+	if len(result) != 4 {
+		t.Error("Expected to find 'а' with a distance of 2")
 	}
 }
